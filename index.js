6ac9fe4e79308493af94a5f8acd8fc1bd2de483a
Muhammad Zeshan <zeshan@zaddns.com>
2023-02-13 15:29:40 +0300

    update code

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ app.post("/sethead", HeadPostApi);
 app.get("/gethead", HeadGetApi);
 
 //................port listen...............
-
-app.listen(5000, () => {
-  console.log("app listening on port 5000!");
-});
+const port = process.env.PORT || 5000;
+app.listen(port, () =>
+  console.log(`Server running on ${port}, http://localhost:${port}`)
+);
